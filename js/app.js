c360e91f6704b7922833654a96d30bb94f5b927b
Sean Connor <seantconnor@gmail.com>
2018-07-14 10:58:52 -0700

    changed map

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {      
+$(document).ready(function() {
    //check if age ID cookie exists
    if($.cookie('warflagbeer.com')) {
       $('#ID').hide();
    } else {
       //check user age
       $('#content').hide();
-      
+
       //the ID form html is served through jquery so it doesn't get included in search engine crawlers which have JS turned off.
       $('#ID').html('<header>\
             <div class="container">\
@@ -22,29 +22,29 @@ $(document).ready(function() {
                 </div>\
             </div>\
          </header>').show();
-      
+
       //if user clicks yes, display content
       $('.yes').on('click', function() {
          //set cookie
          $.cookie('warflagbeer.com','0',{expires: 30});
          //display content
-         
+
          $('#ID').hide();
          $('#content').show();
-         $('.google-maps').html('<iframe src="https://www.google.com/maps/d/embed?mid=z1AgUqVx4gRo.kt_HDdj9rA1w" width="640" height="480"></iframe>').css('display', 'block');
-         
+         // $('.google-maps').html('<iframe src="https://www.google.com/maps/d/embed?mid=z1AgUqVx4gRo.kt_HDdj9rA1w" width="640" height="480"></iframe>').css('display', 'block');
+
       });
 
       //if user click no, do nothing
    };
 
-   
+
    //photo carousel
    $('.carousel').carousel({
     pause: true,
     interval: false
 	});
-   
+
 
 
 });
